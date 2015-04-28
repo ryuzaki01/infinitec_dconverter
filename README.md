b1a67d8d06420d037927e2de30200ff7f30c5d88
ryuzaki01 <shinji666mmc@gmail.com>
2015-04-28 13:08:52 +0700

    + Moved to github

---
+++ README.md
@@ -0,0 +1,56 @@
+Dconverter 1.0
+
+Tools & library yang dibutuhkan :
+1. install java (https://java.com/en/download/)
+2. install libreoffice (https://www.libreoffice.org/)
+3. DocumentConverter Binary (DocumenConverter.jar + libs)
+4. dconverter module
+
+Konfigurasi :
+- Sesuaikan config di file node_modules/dconverter/index.js sesuai path instalasi
+//Absolute path binary DocumentConverter.jar
+var jar = '/Users/yeniovianti/NetBeansProjects/DocumentConverter/dist/DocumentConverter.jar';
+//Absolute path binary libreoffice
+var office = '/Applications/LibreOffice.app/Contents/MacOS/soffice';
+
+
+Fitur :
+- Konversi dari office document file (doc, docx, xls, xlsx, ppt, pptx) ke file pdf
+- Konversi dari office document file (doc, docx, xls, xlsx, ppt, pptx) ke image (png atau jpg)
+- Konversi dari pdf ke image (png atau jpg)
+
+Sample penggunaan :
+- Sample konversi dari office document (doc, docx, xls, xlsx, ppt, pptx) ke file pdf
+
+    var converter = require('dconverter');
+
+    //Input file
+    var input = 'testconvert.docx';
+    //Output directory
+    var outputDir = 'converted/pdf';
+    converter.docTopdf(input,outputDir);
+    
+    //Output file adalah converted/pdf/testconvert.pdf
+    
+- Sample konversi dari office document (doc, docx, xls, xlsx, ppt, pptx) ke file png
+
+    var converter = require('dconverter');
+    
+    //Input file
+    var input = 'testconvert.docx';
+    //Output directory
+    var outputDir = 'converted/pdf';
+    converter.docToImage(input,outputDir,"png");
+    
+    //Output file adalah converted/pdf/testconvert_1.png    
+    
+- Sample konversi dari pdf ke file png
+
+    var converter = require('dconverter');
+    
+    var input = 'testconvert.pdf';
+    var outputDir = 'converted/png';
+    converter.docToImage(input,outputDir,"png");
+    
+    //Output file adalah converted/pdf/testconvert_1.png    
+
---
+++ index.js
@@ -0,0 +1,49 @@
+﻿/**
+ * Created by supardi on 4/23/15.
+ */
+var cli = require("cli");
+
+var jar = '/home/cermati/Infinitec/infinitec_aws/dist/DocumentConverter.jar';
+var office = '/etc/libreoffice/soffice';
+
+
+var dconverter = function(input, output, format){}
+
+dconverter.prototype.pdfToImage = function(input, output, format, callback, failedCallback){
+    var arg = '-jar ' + jar + ' -input ' + input + ' -output ' + output + ' -format ' + format + ' -mode 1';
+    cli.Cli.execute('java', arg, function (command, args, env) {
+        //console.log('Program has been automatically executed. (' + env + ')');
+
+    }, function (command, args, env) {
+        console.error('------------- Error execute on windows try on unix env---------------');
+    }, function (command, args, env) {
+        console.error('------------- Error execute on unix env  ---------------');
+    }, callback, failedCallback);
+};
+
+dconverter.prototype.docToPdf = function(input, output, callback, failedCallback){
+    var args = ['-jar', jar , '-office', office , '-input', input, '-output', output, '-mode', '2'];
+    var env = "mac";
+    cli.Cli.execute('java', args, function (command, args, env) {
+        //console.log('Program has been automatically executed. (' + env + ')');
+
+    }, function (command, args, env) {
+        console.error('------------- Error execute on windows try on unix env  ---------------');
+    }, function (command, args, env) {
+        console.error('------------- Error execute on unix env  ---------------');
+    }, callback, failedCallback);
+};
+
+dconverter.prototype.docToImage = function(input, output, format, callback, failedCallback){
+    var args = ['-jar', jar , '-office', office , '-input', input, '-output', output, '-format', format, '-mode', '3'];
+    cli.Cli.execute('java', args, function (command, args, env) {
+
+    }, function (command, args, env) {
+        console.error('------------- Error execute on windows try on unix env  ---------------');
+    }, function (command, args, env) {
+        console.error('------------- Error execute on unix env  ---------------');
+    }, callback, failedCallback);
+};
+
+convert = new dconverter();
+module.exports = convert;
